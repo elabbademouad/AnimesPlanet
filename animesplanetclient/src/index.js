edfea0808cf4d89bcc358dc4e8987243279831d4
Mouad El abbade <elabbademouad@gmail.com>
2018-05-13 17:58:59 +0000

    create index page client Side

--- animesplanetclient/src/index.js
+++ animesplanetclient/src/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import cover from './cover.jpg'
 class App extends React.Component{
     constructor(){
         super();
-        this.state={Animes:[]};
-        fetch("http://localhost:5000/api/AnimesPlanet")
-        .then(response=>response.json())
-        .then(data=>this.setState({Animes:data}))
-        .catch(err=>console.log(err));
     }
     render(){
-        console.log(this.state.Animes);
-        return <h1>{'Hello WOrld'}</h1>
+        return (
+            <div >
+            <div className='header'>
+                <img className="img-fluid header"  src={cover}/>
+            </div>
+            <Router>
+                <div id="nav">
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/play">Jouer</Link></li>
+                        <li><Link to="/play">Exemples</Link></li>
+                        <li style={{float:'right'}}><Link to="/about">About</Link></li>
+                    </ul>
+                <div>
+                    {/*
+                        <Route exact path="/" component={Home} />
+                        <Route path="/aboust" component={About} />
+                        <Route path="/play" component={Play} />
+                    */}
+                </div>
+                </div>
+            </Router>
+        </div>
+
+        )
     }
 }
 ReactDOM.render(<App />, document.getElementById('root'));
